7029422014ab6e8d2031b7571e55485158e3f074
Kris <kbaltin1@gmail.com>
2020-09-30 12:44:14 -0700

    Started my first Javascript file

---
+++ assignments/week_5/firstjavascript.js
@@ -0,0 +1,42 @@
+// Takes the text in the quotes and sends it to the Javascript Console
+console.log("Hello World!");
+console.log("How are you doing?");
+console.log("Hi");
+
+// This is a single line comment
+
+/* This starts and ends comments, multi-line comments */
+
+// DATATYPES
+// String, spaces and special characters are part of a string. You can't do math on strings.
+"This is a really long sentence. @#$%^&*";
+"To use single quotes ' and ' in the string, use double quotes on the outside.";
+'To use double quotes " and " in the string, use single quotes on the outside.';
+
+// 'There's a nice day!' INCORRECT
+// "There's a nice day!" CORRECT
+
+// The computer will break on the second string.
+// " Hello this is a double quote " "its used for text";
+
+"''''''''''''''''''''";
+
+'""""""""""""""""""""';
+
+
+"100";
+
+console.log(1000);
+
+console.log(true);
+
+console.log("Hey there");
+
+// Number  - numbers not wrapped in double quotes
+100;
+10.9;
+.70009;
+
+// Boolean
+true;
+false;
