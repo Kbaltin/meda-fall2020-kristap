4f2f4ce104833e189de79212c9010d1a21a45931
Kris <kbaltin1@gmail.com>
2020-10-02 09:15:37 -0700

    Added first javascript file

--- assignments/week_5/firstjavascript.js
+++ assignments/week_5/firstjavascript.js
@@ -40,3 +40,108 @@ console.log("Hey there");
 // Boolean
 true;
 false;
+
+
+/* for (var i = 0; i <100000; i++) {
+    console.log("hello");
+ }  */
+
+
+
+//     VARIABLE
+var myBox;
+
+// Naming Schemes
+// Camel Case: capitalize every word AFTER the first word.
+// Example: originalLocation
+// Snake Case: every space is replaced by an underscore and there are no capital letters.
+// Example: original_location
+
+myBox = "books";
+
+var secondVariable = "bathroom supplies";
+
+
+console.log(myBox);
+
+// Mathematical Operators
+// +, -, /, *, % 
+
+var sum = 2 + 4;
+var difference = 2 - 4;
+var quotient = 2 / 4;
+var product = 2 * 4;
+var remainder = 2 % 4;
+
+console.log("The Quotient of 2 and 4 is", quotient);
+console.log(remainder);
+
+// Logical Operator
+// ==. >, <, !=, <=, >=
+
+var isItRaining = true;
+
+isItRaining == true;  // true
+
+var someNumber = 100;
+
+someNumber == 50; // false
+
+someNumber >= 50; //true
+
+someNumber < 50; //false
+
+"hello"  == "hello"; //true
+"goodbye" == "hello"; //false
+"Hello" == "hello"; //false
+"hello " == "hello"; //false
+
+true == true; //true
+true == false; //false
+false == false; //true
+
+sum > product; //false
+
+var personAge = 21;
+
+
+// If Statement, is to run code based on existing data
+//IF    this is true      then run this
+//If        ()                  {}
+
+
+
+
+if (false) {
+    console.log("Nice Day");
+    console.log("Hello there!");   
+}
+if (personAge >= 21) {
+    console.log("Congratz you can drink");
+    } else {
+    console.log("Sorry, you are too young to drink");
+}
+
+console.log("We march on");
+
+// Function
+//  keyword     name of function     code block
+// function       myFunction()            {}
+
+function myFirstFunction()  {
+   
+    console.log("Have a nice day!");
+    console.log("Today will be sunny");
+    console.log("Today will be dry");
+}
+
+    if (raining) {
+        console.log("Bring and umbrella");
+    }
+        
+    var raining = true;
+
+
+myFirstFunction();
+myFirstFunction();
+myFirstFunction();
