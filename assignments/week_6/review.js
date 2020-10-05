be9f4890bab037c5c96b83cb203c9aa399531790
Kris <kbaltin1@gmail.com>
2020-10-05 12:35:14 -0700

    created loops.js file

---
+++ assignments/week_6/review.js
@@ -0,0 +1,44 @@
+var aString = "Today is a really cold day!";
+
+var aNumber = 40;
+
+var aBoolean = true;
+
+//VARIABLES
+var thisIsAReallyLongVariableName;
+
+var b; //bad variable name
+
+var userAge = 39;
+var userAddress;
+
+// IF STATEMENTS
+
+if (userAge > 18) {
+    //Do something if user age is over 18.
+} else {
+    //Doe something if user age is 18 or under.
+}
+
+//FUNCTIONS
+function aFunction() {
+    // Do something when we call  function.
+}
+
+aFunction();
+
+//PARAMETERS & ARGUMENTS
+/*
+function adnotherFunction(parameter1, parameter2) {
+
+}
+
+anotherFunction(arguement1, argument2);
+anotherFunction(arguement1a, argument21);
+
+*/
+
+// RETURNING DATA FROM FUNCTIONS
+/*
+
+function finalFunction() {}
